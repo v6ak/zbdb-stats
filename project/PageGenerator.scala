60ca56a2345ce116c80f867b1e295e23a5048852
Vít Šesták <v6ak@users.noreply.github.com>
2018-09-30 00:28:05 +0200

    Increased endTime for 2018.

--- project/PageGenerator.scala
+++ project/PageGenerator.scala
@@ -74,7 +74,7 @@ object PageGenerator{
     ),
     Year(
       year = 2018, formatVersion = 2017,
-      startTime = "2018-09-21 17:30", endTime = "2018-09-22 20:00",
+      startTime = "2018-09-21 17:30", endTime = "2018-09-22 21:00",
       dataSource = NewGoogleSpreadsheetDataSource("2PACX-1vQyyPGYg-MeFN9nRpHCkog0GKd2h3sSV1lI-EYwMZckmf1w2IV3FSyObWC4GPDKAY6Tu3s46ILm3gN4")
     )
   )
