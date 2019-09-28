9313d13c10853153da774eee2ca4a626f212ab4c
Vít Šesták <v6ak@users.noreply.github.com>
2019-09-28 16:19:52 +0200

    Updated uplaod scripts

--- upload.sh
+++ upload.sh
@@ -7,19 +7,12 @@ set -o pipefail
 
 . upload.config
 
-./pack.sh
-
 branch=$(git rev-parse --abbrev-ref HEAD)
 outdir="$OUT_PATH/$branch"
 echo "outdir: $outdir"
 
 #scp pack.zip "$outdir"
-if [ -e target/all ]; then
-	rm -r target/all
-fi
-mkdir target/all
-(cd target/all; unzip ../../pack.zip)
-ln pack.zip target/all/pack.zip
+./create-target-all
 
 #scp -r target/all/* "$outdir"
 rsync -rvhplt --delete target/all/* "$outdir"
