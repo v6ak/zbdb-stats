c9613c6e1d206fe266a63c7f471937a9c087b0de
Radim Jílek <resan.xorel@gmail.com>
2023-09-20 18:45:44 +0200

    Add ID of public document for year 2023

--- project/PageGenerator.scala
+++ project/PageGenerator.scala
@@ -88,7 +88,7 @@ Year(
     Year(
       year = 2023, formatVersion = 2021,
       startTime = "2023-09-15 17:30", endTime = "2023-09-16 18:00",
-      dataSource= NewGoogleSpreadsheetDataSource("")
+      dataSource= NewGoogleSpreadsheetDataSource("2PACX-1vTzrUrHEarwmtqap2WZQRMJvO7UVy6rGln2xuZv5kWa_slIM6c_-p7BasSUkipAJs86iIOwWDtJlrlb")
     )
   )
 
