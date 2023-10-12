c7bd6e3eaacdcaa6c14d3d37d380b10103dd8190
Vít Šesták <v6ak@users.noreply.github.com>
2023-10-12 12:33:08 +0200

    Get rid of deprecated Stream

--- client/src/main/scala/com/v6ak/zbdb/CollectionUtils.scala
+++ client/src/main/scala/com/v6ak/zbdb/CollectionUtils.scala
@@ -1,9 +1,8 @@
 package com.v6ak.zbdb
 
 object CollectionUtils {
-
-  def addSeparators[T](separator: T)(s: Seq[T]): Seq[T] = (Stream.continually(separator) lazyZip s).flatMap{(x, y) => Seq(x, y)}.drop(1)
-
-  def causeStream(e: Throwable): Stream[Throwable] = Stream.cons(e, Option(e.getCause).fold(Stream.empty[Throwable])(causeStream))
-
+  def addSeparators[T](separator: T)(s: Seq[T]): Seq[T] =
+    (LazyList.continually(separator) lazyZip s).flatMap{(x, y) => Seq(x, y)}.drop(1)
+  def causeStream(e: Throwable): LazyList[Throwable] =
+    LazyList.cons(e, Option(e.getCause).fold(LazyList.empty[Throwable])(causeStream))
 }
