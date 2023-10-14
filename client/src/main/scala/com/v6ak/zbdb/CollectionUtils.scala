396c355d92723757cc87f50e08695ff04baf2740
Vít Šesták <v6ak@users.noreply.github.com>
2023-10-14 21:49:56 +0200

    PlotRenderer: fix warnings

--- client/src/main/scala/com/v6ak/zbdb/CollectionUtils.scala
+++ client/src/main/scala/com/v6ak/zbdb/CollectionUtils.scala
@@ -5,4 +5,8 @@ object CollectionUtils {
     (LazyList.continually(separator) lazyZip s).flatMap{(x, y) => Seq(x, y)}.drop(1)
   def causeStream(e: Throwable): LazyList[Throwable] =
     LazyList.cons(e, Option(e.getCause).fold(LazyList.empty[Throwable])(causeStream))
+
+  implicit class RichMap[K, V](val map: Map[K, V]) extends AnyVal {
+    def mapValuesStrict[W](f: V => W): Map[K, W] = map.view.mapValues(f).toMap
+  }
 }
