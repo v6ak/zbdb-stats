db135e11de2f9ea396dfa52515ac7e387a05c23f
Vít Šesták <v6ak@users.noreply.github.com>
2023-10-22 19:26:30 +0200

    Vite: Main.js

---
+++ assets/statistiky/main.js
@@ -0,0 +1,9 @@
+// CSS import
+import * as css from "./main.scss"
+
+// Imports just for side effects
+import 'bootstrap/js/dist/dropdown'
+import 'chartjs-adapter-moment'
+
+// Import of the main JS file
+import 'client:main.js'
