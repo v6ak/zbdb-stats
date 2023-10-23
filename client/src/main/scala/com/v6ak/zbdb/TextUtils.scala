44c0bf4dca42cfb6114e90f0b24b158edb4e0680
Vít Šesták <v6ak@users.noreply.github.com>
2023-10-23 15:15:56 +0200

    Format TextUtils

--- client/src/main/scala/com/v6ak/zbdb/TextUtils.scala
+++ client/src/main/scala/com/v6ak/zbdb/TextUtils.scala
@@ -1,11 +1,11 @@
 package com.v6ak.zbdb
 
-object TextUtils {
+object TextUtils:
 
-  val Genders = Map[Gender, String](Gender.Male -> "♂", Gender.Female -> "♀")
+  val Genders: Map[Gender, String] = Map(Gender.Male -> "♂", Gender.Female -> "♀")
 
-  def formatLength(length: BigDecimal, space: String = " ") = length.toString().replace('.', ',') + space + "km"
+  def formatLength(length: BigDecimal, space: String = " "): String =
+    length.toString().replace('.', ',') + space + "km"
 
-  def formatSpeed(speedInKmPerH: BigDecimal) = f"$speedInKmPerH%1.2f km/h".replace('.', ',')
-
-}
+  def formatSpeed(speedInKmPerH: BigDecimal): String =
+    f"$speedInKmPerH%1.2f km/h".replace('.', ',')
