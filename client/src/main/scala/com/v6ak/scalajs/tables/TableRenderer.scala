1f4a4d148b04b339ea88a351d36fda59a0a035ff
Vít Šesták <v6ak@users.noreply.github.com>
2023-10-23 14:11:55 +0200

    Enhance expanding/collapsing the table

--- client/src/main/scala/com/v6ak/scalajs/tables/TableRenderer.scala
+++ client/src/main/scala/com/v6ak/scalajs/tables/TableRenderer.scala
@@ -1,17 +1,18 @@
 package com.v6ak.scalajs.tables
 
 import org.scalajs.dom.html.TableRow
-
 import scalatags.JsDom.TypedTag
-import scalatags.JsDom.all._
+import scalatags.JsDom.all.*
 
-final class TableRenderer[T](headRows: Int = 1, tableModifiers: Seq[Modifier] = Seq(), trWrapper: (TypedTag[TableRow], T) => TypedTag[TableRow] = {(a: TypedTag[TableRow], _: T) => a})(columns: Seq[Column[T]]){
+final class TableRenderer[T](
+  headRows: Int = 1,
+  tableModifiers: Seq[Modifier] = Seq(),
+  trWrapper: (TypedTag[TableRow], T) => TypedTag[TableRow] = {(a: TypedTag[TableRow], _: T) => a},
+)(columns: Seq[Column[T]]):
 
-  columns foreach { c =>
-    if(c.rowCount > headRows){  // TODO: consider !=; however, I am not sure about multiple-row cells
+  columns foreach: c =>
+    if(c.rowCount > headRows)  // TODO: consider !=; however, I am not sure about multiple-row cells
       sys.error(s"bad rowCount ${c.rowCount} for $c")
-    }
-  }
 
   def renderTableHead = thead(0 until headRows map { headRowIndex =>
     tr(columns.map{col =>
@@ -21,13 +22,21 @@ final class TableRenderer[T](headRows: Int = 1, tableModifiers: Seq[Modifier] = 
     })
   })
 
-  def renderTableBody(data: Seq[T]) = tbody(data.map(row => trWrapper(tr(columns.map(col => col.createContentCell(row))), row)))
+  def renderTableBody(data: Seq[T]) = tbody(
+    data.zipWithIndex.map( (row, i) =>
+      trWrapper(
+        tr(
+          columns.map(col =>
+            col.createContentCell(row, i, data.size)
+          )
+        ),
+        row
+      )
+    )
+  )
 
   def renderTable(data: Seq[T]) = table(
     renderTableHead,
-    renderTableBody(data)
-  )(
-    tableModifiers : _*
+    renderTableBody(data),
+    tableModifiers,
   ).render
-
-}
